a79478d8c5baee0dbcb4824e44615eac908812b9
matthewchoat <matthewchoat@gmail.com>
2019-08-26 03:52:45 +0000

    ll2

--- linkedlist2.rb
+++ linkedlist2.rb
@@ -1,11 +1,12 @@
-#linkedlist.rb
+#linkedlist2.rb
 
 class LinkedListNode
-  attr_accessor :value, :next_node
+  attr_accessor :value, :next_node, :previous
 
-  def initialize(value, next_node=nil)
+  def initialize(value, next_node=nil, previous=nil)
     @value = value
     @next_node = next_node
+    @previous = value
   end
 end
 
@@ -21,26 +22,36 @@ class Stack
         node = LinkedListNode.new(value, @data)
       else
       node = LinkedListNode.new(value)
+      end
+      @data = node
     end
-    @data = node
-  end
     # Pop an item off the stack.
     # Remove the last item that was pushed onto the
     # stack and return the value to the user
     def pop
       # I RETURN A VALUE
       data1 = @data
-      return nil unless data1 != nil
+      data1.next_node.previous = nil
       @data = data1.next_node
       return data1
     end
-  end
+end
 
-#
+ def infinitecheck(list)
+      stack = Stack.new
+      while list
+        if list.next_node = nil
+          return true
+        end
+        list = list.next_node
+      end
+        return false
+    end
+
   def reverse_list(list)
       stack = Stack.new
       while list
-        stack.push(list.value) 
+        stack.push(list.value)
         list = list.next_node
 
       end
@@ -57,6 +68,10 @@ def print_values(list_node)
     end
   end
 
+def createinfinite(node, first)
+  node.next_node = first
+end
+
 
 node1 = LinkedListNode.new(37)
 node2 = LinkedListNode.new(99, node1)
@@ -71,3 +86,14 @@ puts " "
 puts "-------Reversed List--------"
 reverse_list(node3)
 puts "----------------------------"
+sleep 1.5
+puts " "
+puts "Checking if infinite list..."
+sleep 1.5
+puts " "
+createinfinite(node3, node1)
+if infinitecheck(node3) == false
+  puts "This list is infinite!"
+else 
+  puts "This list is not infinite"
+end
